4391ca600b99b33f8bf25d775de39e24acbae579
hanisntsolo <hanisntsolo@users.noreply.github.com>
2024-08-08 08:47:09 +0000

    deploy: 62dcbd87db6faa8008d4395a35bc4bdea360edad

---
+++ epi_judge_java/epi/CountBits.java
@@ -0,0 +1,23 @@
+package epi;
+import epi.test_framework.EpiTest;
+import epi.test_framework.GenericTest;
+public class CountBits {
+  @EpiTest(testDataFile = "count_bits.tsv")
+
+  public static short countBits(int x) {
+    short countBits = 0;
+    while (x != 0) {
+      x &= (x - 1); // What this does is sets the rightMost setBit to 0
+      countBits++;
+    }
+    return countBits;
+  }
+
+  public static void main(String[] args) {
+    System.exit(
+        GenericTest
+            .runFromAnnotations(args, "CountBits.java",
+                                new Object() {}.getClass().getEnclosingClass())
+            .ordinal());
+  }
+}
