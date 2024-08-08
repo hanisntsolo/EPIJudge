a4c23019a2273b561f79288778a19127db89a713
Dhirendra Pratap Singh <36436712+hanisntsolo@users.noreply.github.com>
2024-08-08 13:30:49 +0530

    first commit

--- epi_judge_java/epi/CountBits.java
+++ epi_judge_java/epi/CountBits.java
@@ -5,8 +5,12 @@ public class CountBits {
   @EpiTest(testDataFile = "count_bits.tsv")
 
   public static short countBits(int x) {
-    // TODO - you fill in here.
-    return 0;
+    short countBits = 0;
+    while (x != 0) {
+      x &= (x - 1); // What this does is sets the rightMost setBit to 0
+      countBits++;
+    }
+    return countBits;
   }
 
   public static void main(String[] args) {
