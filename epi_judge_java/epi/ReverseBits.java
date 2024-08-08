fa36eeaa44b568a15db85dbbd0b6f32f25c58f1f
hanisntsolo <hanisntsolo@users.noreply.github.com>
2024-08-08 19:14:37 +0000

    deploy: fc7e31c048965204f8d4bb8539caf3e5c81f141d

--- epi_judge_java/epi/ReverseBits.java
+++ epi_judge_java/epi/ReverseBits.java
@@ -2,12 +2,36 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class ReverseBits {
+  static long[] preComputeReverse;
+  static final int MASK_SIZE = 16;
+  static final int BIT_MASK = 0xFFFF;
+  static {
+    preComputeReverseArray();
+  }
+  private static void preComputeReverseArray() {
+    preComputeReverse = new long[65536];
+    for (int i = 0; i < 65536; i++) {
+      preComputeReverse[i] = reversePreCompute(i);
+    }
+  }
   @EpiTest(testDataFile = "reverse_bits.tsv")
   public static long reverseBits(long x) {
-    // TODO - you fill in here.
-    return 0;
+    return
+        preComputeReverse[(int)(x                       & BIT_MASK)] << (3 * MASK_SIZE) |
+        preComputeReverse[(int)((x >>> MASK_SIZE)       & BIT_MASK)] << (2 * MASK_SIZE) |
+        preComputeReverse[(int)((x >>> (2 * MASK_SIZE)) & BIT_MASK)] << MASK_SIZE |
+        preComputeReverse[(int)((x >>> (3 * MASK_SIZE)) & BIT_MASK)];
   }
-
+  private static long reversePreCompute(long x) {
+    for (int i = 0; i < MASK_SIZE/2; i++) {
+      x = SwapBits.swapBits(x, i, MASK_SIZE - 1 - i);
+    }
+    return x;
+  }
+  //Helper while creating preComputeArray
+  private static String to16BitBinaryString(int num) {
+    return String.format("%16s", Integer.toBinaryString(num)).replace(' ', '0');
+  }
   public static void main(String[] args) {
     System.exit(
         GenericTest
--- epi_judge_java/epi/SwapBits.java
+++ epi_judge_java/epi/SwapBits.java
@@ -5,7 +5,7 @@ public class SwapBits {
   @EpiTest(testDataFile = "swap_bits.tsv")
   public static long swapBits(long x, int i, int j) {
     if(((x >> i) & 1) != ((x >> j) & 1)) { // This is the case when the bits differ. and then we make a mask and flip them using XOR Operation with mask.
-       return x ^ (1L << i | 1L << j);
+       return x ^ (1L << i | 1L << j); // (1L << i | 1L << j) this is the mask for flipping.
     }
     return x;
   }
