7397e9da4940419e123e86ba8ea36e55dbdfc0d3
Dhirendra Pratap Singh <36436712+hanisntsolo@users.noreply.github.com>
2024-08-08 20:51:29 +0530

    count bits done

--- epi_judge_java/epi/CountBits.java
+++ epi_judge_java/epi/CountBits.java
@@ -3,7 +3,7 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class CountBits {
   @EpiTest(testDataFile = "count_bits.tsv")
-
+// Using O(k) totalNumberOf set bits
   public static short countBits(int x) {
     short countBits = 0;
     while (x != 0) {
@@ -12,6 +12,17 @@ public class CountBits {
     }
     return countBits;
   }
+  // Using O(n) total number of bits
+//  public static short countBits(int x) {
+//    short countBits = 0;
+//    while (x != 0) {
+//      if((1 & x) > 0) {
+//        countBits++;
+//      }
+//      x >>= 1;
+//    }
+//    return countBits;
+//  }
 
   public static void main(String[] args) {
     System.exit(
