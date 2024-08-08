813ecb5eadeaa907b8106d4477f00aecb883a06e
hanisntsolo <hanisntsolo@users.noreply.github.com>
2024-08-08 15:44:39 +0000

    deploy: 9bdb98740154c2e2c5925b310315429526cbadf7

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -2,10 +2,27 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class Parity {
+  // O(n) running time
+//  @EpiTest(testDataFile = "parity.tsv")
+//  public static short parity(long x) {
+//    short countBits = 0;
+//    while(x != 0) {
+//      if((x & 1) == 1) {
+//        countBits++;
+//      }
+//      x >>= 1;
+//    }
+//    return (short) (countBits % 2);
+//  }
+  // O(k) running time focussed on the set bits only using bit fiddling.
   @EpiTest(testDataFile = "parity.tsv")
-  public static short parity(long x) {
-    // TODO - you fill in here.
-    return 0;
+  public static short parityBitFiddling(long x) {
+    short parity = 0;
+    while(x != 0) {
+      parity ^= 1; // To track even or odd based on each iter.
+      x = x & (x - 1); // Sets last bit to 0 based on each iter.
+    }
+    return parity;
   }
 
   public static void main(String[] args) {
