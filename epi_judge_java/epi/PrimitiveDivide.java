3cbcf9d6fa8184a33c69d52ada17f378c5a6135f
Dhirendra Pratap Singh <36436712+hanisntsolo@users.noreply.github.com>
2024-08-10 22:25:32 +0530

    removed lombok as it was breaking build for java on g-actions

--- epi_judge_java/epi/PrimitiveDivide.java
+++ epi_judge_java/epi/PrimitiveDivide.java
@@ -1,8 +1,7 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.logging.Logger;
-import lombok.extern.slf4j.Slf4j;
+
 
 public class PrimitiveDivide {
   // BRUTE FORCE // WORST RUNNING TIME.
