89396330e870cd42b265b405ceb72614a04e3b63
Dhirendra Pratap Singh <36436712+hanisntsolo@users.noreply.github.com>
2024-10-30 12:53:56 +0530

    Stacks done

--- epi_judge_java/epi/EvaluateRpn.java
+++ epi_judge_java/epi/EvaluateRpn.java
@@ -1,12 +1,31 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
+import java.util.function.ToIntBiFunction;
+
 public class EvaluateRpn {
   @EpiTest(testDataFile = "evaluate_rpn.tsv")
 
   public static int eval(String expression) {
-    // TODO - you fill in here.
-    return 0;
+    Deque<Integer> intermediateResults = new ArrayDeque<>();
+    String DELIMITER = ",";
+    final Map<String, ToIntBiFunction<Integer, Integer>> OPERATORS = Map.of( // SMART
+    "+", (y, x) -> x + y,
+    "-", (y, x) -> x - y,
+    "/", (y, x) -> x / y,
+    "*", (y, x) -> x * y
+    );
+    for(String token : expression.split(DELIMITER)) {
+      if(OPERATORS.get(token) != null) {
+        intermediateResults.addFirst(OPERATORS.get(token).applyAsInt(intermediateResults.removeFirst(), intermediateResults.removeFirst()));
+      } else {
+        intermediateResults.addFirst(Integer.parseInt(token));
+      }
+    }
+    return intermediateResults.removeFirst();
   }
 
   public static void main(String[] args) {
--- epi_judge_java/epi/SunsetView.java
+++ epi_judge_java/epi/SunsetView.java
@@ -2,13 +2,32 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 public class SunsetView {
+  static class BuildingWithHeight {
+    Integer id;
+    Integer height;
+    public BuildingWithHeight(Integer id, Integer height) {
+      this.id = id;
+      this.height = height;
+    }
+  }
   public static List<Integer>
   examineBuildingsWithSunset(Iterator<Integer> sequence) {
-    // TODO - you fill in here.
-    return null;
+    int buildingIdX = 0;
+    Deque<BuildingWithHeight> buildings = new ArrayDeque<>();
+    while (sequence.hasNext()) {
+      Integer buildingHeight = sequence.next();
+      while(!buildings.isEmpty() && buildingHeight >= buildings.peekFirst().height) {
+        buildings.removeFirst();
+      }
+      buildings.addFirst(new BuildingWithHeight(buildingIdX++, buildingHeight));
+    }
+    return buildings.stream().map(tower -> tower.id).toList();
   }
   @EpiTest(testDataFile = "sunset_view.tsv")
   public static List<Integer>
