533cafc9358bc43976032027e68d9985557938dd
hanisntsolo <hanisntsolo@users.noreply.github.com>
2024-11-01 16:27:31 +0000

    deploy: 0d10dd5fab85581a9cbffd80c7fe1194bfb0d380

--- epi_judge_java/epi/IsTreeBalanced.java
+++ epi_judge_java/epi/IsTreeBalanced.java
@@ -3,13 +3,37 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class IsTreeBalanced {
 
-  @EpiTest(testDataFile = "is_tree_balanced.tsv")
+  static class BalancedStatusWithHeight {
 
-  public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
+    private boolean isBalanced;
+    private int height;
+
+    public BalancedStatusWithHeight(boolean isBalanced, int height) {
+      this.isBalanced = isBalanced;
+      this.height = height;
+    }
+    private static BalancedStatusWithHeight checkBalanced(BinaryTreeNode<Integer> tree) {
+      if (tree == null) {
+        return new BalancedStatusWithHeight(true/*null tree is always height balanced*/,
+            -1/*Height is going to be -1 for null tree*/);
+      }
+      BalancedStatusWithHeight left = checkBalanced(tree.left);
+      if (!left.isBalanced) {
+        return left;
+      }
+      BalancedStatusWithHeight right = checkBalanced(tree.right);
+      if (!right.isBalanced) {
+        return right;
+      }
+      boolean isBalanced = Math.abs(left.height - right.height) <= 1;
+      int height = Math.max(left.height, right.height) + 1;
+      return new BalancedStatusWithHeight(isBalanced, height);
+    }
   }
-
+  @EpiTest(testDataFile = "is_tree_balanced.tsv")
+  public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
+    return BalancedStatusWithHeight.checkBalanced(tree).isBalanced;
+  }
   public static void main(String[] args) {
     System.exit(
         GenericTest
--- epi_judge_java/epi/SumRootToLeaf.java
+++ epi_judge_java/epi/SumRootToLeaf.java
@@ -5,8 +5,17 @@ public class SumRootToLeaf {
   @EpiTest(testDataFile = "sum_root_to_leaf.tsv")
 
   public static int sumRootToLeaf(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return 0;
+    return sumRootToLeafHelper(tree, 0/*partial path sum*/);
+  }
+  private static int sumRootToLeafHelper(BinaryTreeNode<Integer> tree, int partialPathSum) {
+    if(tree == null) {
+      return 0;
+    }
+    partialPathSum = partialPathSum * 2 + tree.getData();
+    if(tree.left == null && tree.right == null) { // Case of being leaf node.
+      return partialPathSum;
+    }
+    return sumRootToLeafHelper(tree.left, partialPathSum) + sumRootToLeafHelper(tree.right, partialPathSum);
   }
 
   public static void main(String[] args) {
